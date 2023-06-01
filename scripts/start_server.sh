d4e6819d9c2bab44f9274eb31b2fd752366955fe
akshatarora02 <akshatarora028@gmail.com>
2023-06-01 15:39:10 +1000

    made changes to docker

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+##remove previously running containers
+docker-compose down /var/www/html/myapp/docker-compose.yml --remove-orphans
+
 docker-compose -f /var/www/html/myapp/docker-compose.yml up -d
