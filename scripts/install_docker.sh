7a47f196af62b1025feb10d5c984f489c92d6f8d
akshatarora02 <akshatarora028@gmail.com>
2023-06-01 15:15:10 +1000

    add docker and docker-compose install

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 # Install Docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+yum update -y
+yum install docker -y
+usermod -a -G docker ec2-user
+id ec2-user
+newgrp docker
+
+
+yum install python3-pip
+pip3 install docker-compose
