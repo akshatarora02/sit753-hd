b4dba2d1f934396fae2399a5f2bea5fc45d21f59
akshatarora02 <akshatarora028@gmail.com>
2023-06-01 16:16:33 +1000

    fix docker-compose down command

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ##remove previously running containers
-docker-compose down -f /var/www/html/myapp/docker-compose.yml --remove-orphans
+docker-compose -f /var/www/html/myapp/docker-compose.yml down --remove-orphans
 
 docker-compose -f /var/www/html/myapp/docker-compose.yml up -d
